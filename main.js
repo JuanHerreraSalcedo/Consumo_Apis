03b64e6c7aff0df8b92ac3f29cc47b43f2eb5ab2
JHerrera5 <jherrera@existaya.com>
2022-06-28 12:24:12 -0500

    Ya aparecen los michis favoritos

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c4afcdf7-aa0d-4e2f-a0cd-35fd2769d5d8';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c4afcdf7-aa0d-4e2f-a0cd-35fd2769d5d8';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?&api_key=c4afcdf7-aa0d-4e2f-a0cd-35fd2769d5d8';
 
 const spanError = document.getElementById('Error')
 
@@ -14,35 +14,51 @@ async function loadRandomMichis() {
     } else {
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
+        const btn1 = document.getElementById('btn1')
+        const btn2 = document.getElementById('btn2')
 
         img1.src = data[0].url;
         img2.src = data[1].url;
 
-        if (res.status !== 200) {
-            spanError.innerHTML = "Hubo un error: " + res.status + data.message;
-        }
+        btn1.onclick = () => saveFavouriteMichi(data[0].id);
+        btn2.onclick = () => saveFavouriteMichi(data[1].id);
     }
 }
 
 async function loadFavouriteMichis() {
-    const res = await fetch(API_URL_RANDOM);
+    const res = await fetch(API_URL_FAVORITES);
     const data = await res.json();
     console.log('Favoritos')
     console.log(data)
 
     if (res.status !== 200) {
         spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+    } else {
+        data.forEach(michi => {
+            const section = document.getElementById('favoritesMichis');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+            img.src = michi.image.url
+            img.width = 150;
+            btn.appendChild(btnText);
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        });
     }
 }
 
-async function saveFavouriteMichis() {
+async function saveFavouriteMichi(id) {
     const res = await fetch(API_URL_FAVORITES, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            image_id: 'yD8LO5iJO'
+            image_id: id
         }),
     });
 
